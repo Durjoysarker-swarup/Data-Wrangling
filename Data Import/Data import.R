da1fb3ebf167345e1565b6157259d54487917542
Durjoy Sarker <durjoysarker78@gmail.com>
2025-08-10 05:58:55 +0600

    2nd day of learning

--- Data Import/Data import.R
+++ Data Import/Data import.R
@@ -35,7 +35,12 @@ read_lines("murders.csv" , n_max=3)
 
 #read file in csv format
 data <- read_csv("murders.csv")
+class(data)
 head(data)
+
+#so by reading with the R default function the dataset is just the dataframe.
+data2 <- read.csv("murders.csv")
+class(data2)
 
 #another way to find the file path
 file_path <- file.path(path, "murders.csv" )
@@ -48,6 +53,29 @@ sheets <- excel_sheets("data.xlsx")
 read_excel("data.xlsx", sheet = "Sales")
 df <- read_excel("data.xlsx", sheet = sheets[2])
 
+# 1. Save URL
+url <- "https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
 
+# 2. Read CSV directly from the web
+dat1 <- read_csv(url)
+
+# 3. Download CSV file permanently in working directory
+download.file(url, "murders.csv")
+
+# 4. Create temporary filename
+tmp_filename <- tempfile()
+
+# 5. Download CSV file to temporary location
+download.file(url, tmp_filename)
+
+# 6. Read CSV from temporary file
+dat2 <- read_csv(tmp_filename)
+
+# 7. Delete temporary file
+file.remove(tmp_filename)
+
+# Check your data
+print(head(dat1))
+print(head(dat2))
 
 
