a8300bac598f0f164f2884b65443b3195b2b8977
Durjoy Sarker <durjoysarker78@gmail.com>
2025-08-10 05:09:23 +0600

    learning the second lecture

--- Data Import/Data import.R
+++ Data Import/Data import.R
@@ -25,5 +25,29 @@ file.exists("murders.csv")
 
 
 
+library(dslabs)
+library(tidyverse)    # includes readr
+library(readxl)
+
+#without reading the data just see the value
+read_lines("murders.csv" , n_max=3)
+
+
+#read file in csv format
+data <- read_csv("murders.csv")
+head(data)
+
+#another way to find the file path
+file_path <- file.path(path, "murders.csv" )
+
+#read the excel file
+# Get sheet names
+sheets <- excel_sheets("data.xlsx")
+
+# Read a specific sheet
+read_excel("data.xlsx", sheet = "Sales")
+df <- read_excel("data.xlsx", sheet = sheets[2])
+
+
 
 
