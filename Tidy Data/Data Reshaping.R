2019cd3bf24398ebebcecfbbf9bf36b6e30a8ec1
Durjoy Sarker <durjoysarker78@gmail.com>
2025-08-12 23:14:54 +0600

    learning the pivot_longer

---
+++ Tidy Data/Data Reshaping.R
@@ -0,0 +1,42 @@
+#Key points
+#After importing data, a common next step is to reshape the data into a form useful for the rest of the analysis by tidying it. The tidyr package includes several useful functions for tidying data.
+#The pivot_longer() function converts wide data into tidy data.
+#The first argument of pivot_longer() is the data frame to be reshaped. The second argument specifies the columns containing the values to be moved into a single column.
+#The new column of values is called value by default and the column containing the original names of those columns is called name by default.
+#The values_to and names_to arguments can be used to change the default names of these columns.
+
+library(tidyverse)
+library(dslabs)
+
+path <- system.file("extdata", package= "dslabs")
+list.files(path)
+filename <- file.path(path, "fertility-two-countries-example.csv" )
+wide_data <- read_csv(filename)
+
+#just snippet of the wide data
+wide_data %>% select(country, `1960`:`1965`)
+
+#pivot the column into one column
+wide_data %>% pivot_longer(`1960`:`2015`)
+
+#another way of doing this
+wide_data%>% pivot_longer(-country)
+#here you tell without the country all will be changed
+
+#change the default column name
+new_tidy_data <- wide_data %>%
+  pivot_longer(-country, names_to = "year", values_to = "fertility rate")
+new_tidy_data
+
+class(new_tidy_data$year)
+
+#use the name_transform argument to change the class of the year as value to numeric
+new_tidy_data <- wide_data %>% 
+  pivot_longer(-country, names_to = "year", values_to = "fertility rate", names_transform = list(year=as.numeric))
+new_tidy_data
+
+
+#ploting the data
+new_tidy_data %>% ggplot(aes(year, `fertility rate`, color=country)) +
+  geom_point()
+
