bb4f0176301466e111aeeb67c84a87439a174701
Durjoy Sarker <durjoysarker78@gmail.com>
2025-08-16 14:11:02 +0600

    hi

--- Tidy Data/Data Reshaping/Data Reshaping.R
+++ Tidy Data/Data Reshaping/Data Reshaping.R
@@ -49,3 +49,62 @@ new_tidy_data %>% ggplot(aes(year, `fertility rate`, color=country)) +
 new_wide_data <- new_tidy_data %>% 
   pivot_wider(names_from = year, values_from = `fertility rate`)
 new_wide_data
+
+
+##separate
+#import data
+path <- system.file("extdata", package = "dslabs")
+fname <-  "life-expectancy-and-fertility-two-countries-example.csv"
+filename <- file.path(path, fname)
+
+raw_data <- read.csv(filename)
+select(raw_data, 1:4)
+
+#pivot all column expect country
+dat <- raw_data %>% pivot_longer(-country)
+dat
+
+#separate on underscore
+dat %>% separate(name, c("year", "name"), sep="_")
+
+# separate on underscores (the default), convert years to numeric
+dat %>%  separate(name, c("year", "name"), sep = "_") %>%
+  mutate(year = as.integer(str_remove(year, "^X")))
+
+# split on all underscores, pad empty cells with NA
+dat %>% separate(name, c("year", "name_1", "name_2"), 
+                 fill = "right", convert = TRUE)
+
+
+# split on first underscore but keep life_expectancy merged
+dat %>% separate(name, c("year", "name"), sep = "_", 
+                 extra = "merge", convert = TRUE)
+
+# separate then create a new column for each variable using pivot_wider
+dat %>% separate(name, c("year", "name"), sep = "_", 
+                 extra = "merge", convert = TRUE) %>%
+  pivot_wider()
+
+
+# if we had used this non-optimal approach to separate
+dat %>% 
+  separate(name, c("year", "name_1", "name_2"), 
+           fill = "right", convert = TRUE)
+
+# we could unite the second and third columns using unite()
+dat %>% 
+  separate(name, c("year", "name_1", "name_2"), 
+           fill = "right", convert = TRUE) %>%
+  unite(variable_name, name_1, name_2, sep="_")
+
+# spread the columns
+dat %>% 
+  separate(name, c("year", "name_1", "name_2"), 
+           fill = "right", convert = TRUE) %>%
+  unite(name, name_1, name_2, sep="_") %>%
+  spread(name, value) %>%
+  rename(fertlity = fertility_NA)
+
+
+
+
